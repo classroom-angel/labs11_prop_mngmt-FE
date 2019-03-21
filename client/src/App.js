fa1ce3b72b397cc27dc3852a1f6af78cc6be7856
johnoro <officialjohnorourke@gmail.com>
2019-03-21 02:54:24 -0400

    client/src/App: adds inputs for hooks example and api usage

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
+import axios from './axiosInstance';
+import useFormInput from './components/useFormInput';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = props => {
+  const inputs = [
+    useFormInput('Username'),
+    useFormInput('First name'),
+    useFormInput('Last name'),
+    useFormInput('Password')
+  ];
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    const tempResponse = await axios.post(`users/register`);
+
+    console.log(tempResponse);
+
+    inputs.forEach(input => {
+      input.value = '';
+    })
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <form onSubmit={onSubmit}>
+          <>
+            {
+              inputs.map(input => (
+                <input {...input} />
+              ))
+            }
+          </>
+          <button>Submit</button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
