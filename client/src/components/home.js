511e6429cb349c6097076569c2c2a8c23e6b2e17
Rachel DiCesare <racheldicesare93@gmail.com>
2019-03-21 17:26:09 -0400

    added navlinks and title to home

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './home.css'
+// import onBoard from './components/onBoard';
 
 function home() {
     return (
+        <div>
         <div className="onboarding-component">
-            <h1>Could house the singup, landing page, homepage, etc...</h1>
+            <h1>Classroom Angel</h1>
+            <NavLink to="/ourmission">Our Mission</NavLink>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+            <NavLink to="/signout">Signout</NavLink>
+        </div>
+        <div className = "Main">
+         <NavLink to="/onBoard">Get Started</NavLink>
+         </div>
+       
+        <div className= "Footer">
+        <p>©Classroom Angel</p>
+         <NavLink to="/team">Meet The Team</NavLink>
+        </div>
         </div>
     )
 }
