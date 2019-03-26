47c067314e87be70ffc6e4adc0d2b5109c1ce777
johnoro <officialjohnorourke@gmail.com>
2019-03-26 13:51:34 -0400

    Adds outline for JoinOrg component

--- client/src/components/JoinOrg.js
+++ client/src/components/JoinOrg.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function JoinOrg() {
+function JoinOrg(props) {
     return (
         <div className="onboarding-component">
-            <h1>Join organization Component</h1>
+            <h1>What's the name of your organization?</h1>
+            
+            {/* SEARCH BAR */}
+            <form>
+                {/* Magnifying glass */}
+                <input 
+                    onChange={props.onChange}
+                    type="text"
+                    placeholder="search"
+                />
+                {/* Microphone */}
+            </form>
+            
+            {/* BODY */}
+            {/* Organization list */}
+            {/* Vertical scroll bar */}
+
+            {/* CREATE ORGANIZATION */}
+            <p>Don't see your organization? <Link >Add yours here</Link></p>
         </div>
     )
 }
