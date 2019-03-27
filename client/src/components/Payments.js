f7435f25f8b57a0bb9a08e339e08ae5647fd326a
Rachel DiCesare <racheldicesare93@gmail.com>
2019-03-27 16:54:13 -0400

    added stripe integration for FE, going to work on BE

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Sidebar from './Sidebar';
+import Checkout from './Checkout';
 import '../App.css'
 
 function Payments() {
     return (
         <div className="page-container">
-            <Sidebar />
-            <div>Payments stuff....</div>
+       <Sidebar />
+            <div className="checkout">
+            <Checkout
+              name={'10+ Teachers'}
+              description={'Subscribe to get more teachers'}
+              amount={5.00}
+           />
+        </div>
         </div>
     )
 }
