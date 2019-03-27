aeef0f8f1746b173e592819c589722e7bfc279dd
Alec <alec.jordan.cs@gmail.com>
2019-03-27 16:22:26 -0400

    adds visits routes updates react-router in App.js

--- client/src/components/Visits.js
+++ client/src/components/Visits.js
@@ -2,56 +2,70 @@ import React from 'react'
 import Sidebar from './Sidebar';
 import '../App.css'
 import axios from '../axiosInstance'
+import {NavLink} from 'react-router-dom'
 
-export default class Visits extends React.Component {
+const statuses = [
+    "Needs Attention",
+    "Resolved",
+    "Scheduled"
+]
+export default class IssueLog extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            visits: [],
-            visitsLoaded: false,
-            visitName: "",
-            visitNotes: "",
-            visitStatus: "Needs Attention",
+            issues: [],
+            issuesLoaded: false,
+            issueName: "",
+            issueNotes: "",
+            issueStatus: "Needs Attention",
             orgID: 1,
-            editingVisit: false,
-            visit: null 
+            editingIssue: false,
+            issue: null 
         }
-        this.postVisits = this.postVisits.bind(this)
-        this.deleteVisit = this.deleteVisit.bind(this)
+        this.postIssues = this.postIssues.bind(this)
+        this.deleteIssue = this.deleteIssue.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.toggleEdit = this.toggleEdit.bind(this)
-        this.fetchVisit = this.fetchVisit.bind(this)
+        this.fetchIssue = this.fetchIssue.bind(this)
     }
 
     componentDidMount() {
-        axios.get('visits').then(res => this.setState({visits: res.data.visits, visitsLoaded: true})).catch(err => console.log(err))
+        axios
+        .get('issues')
+        .then(res => {
+            let copy = res.data.issues.filter(function(issue) {
+                return issue.is_visit
+            })
+            this.setState({issues: copy, issuesLoaded: true})
+        })
+        .catch(err => console.log(err))
     }
 
-    postVisits(event) {
+    postIssues(event) {
         console.log('posting...')
         event.preventDefault()
-        axios.post('visits', {name: this.state.visitName,
-          notes: this.state.visitNotes,
-        status: this.state.visitStatus,
+        axios.post('issues', {name: this.state.issueName,
+          notes: this.state.issueNotes,
+        status: this.state.issueStatus,
         isVisit: false,
         organizationId: this.state.orgID,
         date: '03-25-20'
      })
        .then(res => {
            console.log(res)
-           this.setState({visitName: "", visitNotes: "", visits: [...this.state.visits, res.data.visit]})
+           this.setState({issueName: "", issueNotes: "", issues: [...this.state.issues, res.data.issue]})
        })
        .catch(err => console.log(err))
     }
 
-    deleteVisit(event) {
-        axios.delete(`visits/${event.target.value}`)
+    deleteIssue(event) {
+        axios.delete(`issues/${event.target.value}`)
         .then(res => {
-            console.log(res.data.visit.id)
-            var copy = this.state.visits.filter(function(element) {
-                return element.id !== res.data.visit.id 
+            console.log(res.data.issue.id)
+            var copy = this.state.issues.filter(function(element) {
+                return element.id !== res.data.issue.id 
             })
-            this.setState({visits: copy})
+            this.setState({issues: copy})
         })
         .catch(err => console.log(err))
       }
@@ -62,17 +76,17 @@ export default class Visits extends React.Component {
 
       toggleEdit() {
         this.setState({
-          editingVisit: !this.state.editingVisit,
+          editingIssue: !this.state.editingIssue,
           title: this.state.note.title,
           textBody: this.state.note.textBody
         })
     }
 
-    fetchVisit(id) {
-        axios.get(`visits/${id}`)
+    fetchIssue(id) {
+        axios.get(`issues/${id}`)
         .then(res => {
             console.log('fetched note', res.data)
-            this.setState({visit: res.data.visit})
+            this.setState({issue: res.data.issue})
         })
         .catch(err => {
             console.log(err)
@@ -81,32 +95,38 @@ export default class Visits extends React.Component {
     
 
     render() {
-    if (this.state.visitsLoaded) {
+    if (this.state.issuesLoaded) {
         return (
             <div className="page-container">
                 <Sidebar />
                 <div className="right-side">
                     <h1 style={{textAlign: 'center', border: '2px solid gray'}}>Visits</h1>
                     <ul>
-                        {this.state.visits.map(visit => {
+                        {this.state.issues.map(issue => {
                             return (
-                                <div key={visit.id}>
-                                  <h1>Name: {visit.name}</h1>
-                                  <h2>Notes: {visit.notes}</h2>
-                                  <h3>Status: {visit.status}</h3>
-                                  <h4>Date: {visit.date}</h4>
-                                  <h5>Org. Id: {visit.organization_id}</h5>
-                                  <button onClick={this.deleteVisit} value={visit.id} sytle={{backgroundColor:'firebrick', color:'orange'}}>Delete Visit</button>
-                                  <button onClick={this.deleteVisit} value={visit.id} sytle={{backgroundColor:'firebrick', color:'orange'}}>Delete Visit</button>
+
+                                <div key={issue.id} className="issue-card">
+                                  <h1>Name: {issue.name}</h1>
+                                  <h2>Notes: {issue.notes}</h2>
+                                  <h3>Status: {issue.status}</h3>
+                                  <h4>Date: {issue.date}</h4>
+                                  <h5>Org. Id: {issue.organization_id}</h5>
+                                  <button onClick={this.deleteIssue} value={issue.id} sytle={{backgroundColor:'firebrick', color:'orange'}}>Delete Issue</button>
+                                  <NavLink to={`/issue/${issue.id}`}><div value={issue.id} className="edit-issue-button">Update Issue</div></NavLink>
                                 </div>
-                            )
+                            ) 
                         })}
                     </ul>
-                    <form onSubmit={this.postVisits}>
-                        <input name="visitName" value={this.state.visitName} placeholder="Visit Title" onChange={this.handleChange}/>
-                        <input name="visitNotes" value={this.state.visitNotes} placeholder="Additional notes" onChange={this.handleChange}/>
-                        {/* Need to come back and use select elements for the following line */}
-                        <input name="visitStatus" value={this.state.visitStatus} placeholder="Status" onChange={this.handleChange}/> 
+                    <form onSubmit={this.postIssues}>
+                        <input name="issueName" value={this.state.issueName} placeholder="Issue Title" onChange={this.handleChange}/>
+                        <input name="issueNotes" value={this.state.issueNotes} placeholder="Additional notes" onChange={this.handleChange}/>
+                        <select name="role" onChange={this.change} value={this.state.role}>
+                            <option>Status...</option>
+                                {statuses.map(status => {
+                                  return <option value={status}>{status}</option>
+                                })}
+                        </select>
+                        
                         <input type="submit" />
                     </form>
                 </div>
