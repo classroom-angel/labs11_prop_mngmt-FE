619a12fe1454d6b0b688d63d8b707915db291ed3
Alec <alec.jordan.cs@gmail.com>
2019-04-03 13:37:25 -0400

    update bm-homepage

--- client/src/components/BoardMember/BoardMember.js
+++ client/src/components/BoardMember/BoardMember.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import '../../App.css'
 // import './BoardMember.css'
-import axios from '../../axiosInstance'
+import axios from '../../axiosInstance' 
 
 export default class BoardMemberHub extends React.Component {
     constructor(props) {
@@ -28,38 +28,73 @@ export default class BoardMemberHub extends React.Component {
         .then(res => this.setState({issues: res.data.issues, issuesLoaded: true}))
         .catch(err => console.error(err))
 
-        axios
-        .get('attendance')
-        .then(res => this.setState({attendance: res.data.attendance, attendanceLoaded: true}))
-        .catch(err => console.error(err))
+        // axios
+        // .get('attendance')
+        // .then(res => this.setState({attendance: res.data.attendance, attendanceLoaded: true}))
+        // .catch(err => console.error(err))
     }
 
     render() {
         return (
-            <div className="page-container grid-container">
+            <div className="page-container">
                     <Sidebar/>
-                    <div className="right-side grid-container">
-                        <div className="item-1" style={{display: 'inline-block', border: '2px solid'}}>
+                    <div className="right-side">
+                    <h1 style={{textAlign: 'left'}}>School Name Here</h1>
+                        <div className="bm-issues" style={{display: 'inline-block', margin: '20px 5%', width: '45%', border: '5px solid firebrick', borderRadius: '10px'}}>
+                        <h2 style={{textAlign:'left'}}>Issue Log</h2>
+                        <div style={{overflow: 'scroll', height: '500px'}}>
+                        {
+                                this.state.issuesLoaded ? (
+                                    this.state.issues.map(function(issue) {
+                                        return (
+                                        <p key={issue.id}><span style={{margin:'2px 10px'}}> {issue.name}</span><span style={{margin:'2px 10px'}}>{issue.date}</span><span style={{margin:'2px 10px'}}>{issue.status.toUpperCase()}</span></p>
+                                        )
+                                    }))
+                                : "Loading..."
+                            }
+                            </div>
+                        </div>
+                        <div className="bm-devices" style={{display: 'inline-block', border: '2px solid', height: '500px'}}>
+                        <div className="dev-condiiton">
+                        <p>Equipment Open Issue Working Total</p>
                             {
                                 this.state.equipmentLoaded ? (
                                     this.state.equipment.map(function(item) {
                                         return (
                                         <div key={item.id}>
-                                        <p>ID: {item.id}</p>
-                                        <p>Name: {item.name}</p>
-                                        <p>Description: {item.description}</p>
-                                        <p>Working: {item.working}</p>
-                                        <p>Damaged: {item.damaged}</p>
-                                        <p>OrgID: {item.organizationId}</p>
-                                        <p>Total: {item.working + item.damaged}</p>
+                                        <p>{item.id} {item.name} {item.working} {item.damaged} {item.working + item.damaged}</p>
+                                        {/* <p>OrgID: {item.organizationId}</p> */}
                                         </div>
                                         )
                                     }))
                                 : "Loading..."
                             }
                         </div>
-                        <div className="item-2" style={{display: 'inline-block', border: '2px solid'}}>
-                            {
+                        <div className="dev-description" style={{display: 'inline-block', border: '2px solid'}}>
+                           {
+                               this.state.equipmentLoaded ? (
+                                   this.state.equipment.map(function(item) {
+                                       return (
+                                           <div>
+                                                <p>{item.name}</p>
+                                                <p>Description: {item.description}</p>
+                                           </div>
+                                           
+                                       )
+                                   })
+                               ): "Loading...."
+                                }
+                        </div>
+                        </div>
+                        
+                    </div>
+            </div>
+        )
+
+    }
+}
+
+ /* {
                                 this.state.attendanceLoaded ? (
                                     this.state.attendance.map(function(teacher) {
                                         return (
@@ -74,26 +109,4 @@ export default class BoardMemberHub extends React.Component {
                                         )
                                     }))
                                 : "Loading..."
-                            }
-                        </div>
-                        <div className="item-3" style={{display: 'inline-block', border: '2px solid'}}>
-                            {
-                                this.state.issuesLoaded ? (
-                                    this.state.issues.map(function(issue) {
-                                        return (
-                                        <div key={issue.id}>
-                                        <p>ID: {issue.id}</p>
-                                        <p>name: {issue.name}</p>
-                                        <p>Date: {issue.date}</p>
-                                        </div>
-                                        )
-                                    }))
-                                : "Loading..."
-                            }
-                        </div>
-                    </div>
-            </div>
-        )
-
-    }
-}
+                            } */
