cf6be8a165bd89fbab0ffc3046a78ddad34163ce
johnoro <officialjohnorourke@gmail.com>
2019-04-05 12:07:03 -0400

    Adds unified checkout component for less confusion and less repetition

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,90 +1,112 @@
-import React from 'react'
-import Connect from './Connect';
+import React from 'react';
 import Sidebar from './Sidebar/Sidebar';
-import Checkout from './Checkout';
+import Checkout, { subscribe, connect } from './Checkout';
 import axios from '../axiosInstance';
-import '../App.css'
+import '../App.css';
 
 class Payments extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      amount: "",
-      accountId: ""
-    }
+      amount: '',
+      accountId: ''
+    };
   }
 
   componentDidMount = () => {
     this.getCredentials();
     const { history } = this.props;
-    history.push("/");
-    history.push("/payments");
-    let code = localStorage.getItem("code");
+    history.push('/');
+    history.push('/payments');
+    let code = localStorage.getItem('code');
     let sendObj = {
       code
-    }
+    };
 
     if (code) {
-      axios.post('/payment/connect', sendObj)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+      axios
+        .post('/payment/connect', sendObj)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
-  }
+  };
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: [e.target.value]})
-  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: [e.target.value] });
+  };
 
   getCredentials = () => {
     let vars = {};
-    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-    });
+    let parts = window.location.href.replace(
+      /[?&]+([^=&]+)=([^&]*)/gi,
+      function(m, key, value) {
+        vars[key] = value;
+      }
+    );
 
-    const code = vars["code"];
+    const code = vars['code'];
     console.log(code);
-    localStorage.setItem("code", code)
+    localStorage.setItem('code', code);
     return code;
+  };
 
-  }
+  fromDollarToCent = amount => amount * 100;
 
   render() {
     return (
-        <div className="page-container">
-          <Sidebar />
-            <div className="checkout right-side">
-
-            <h1>Upgrade Account</h1>
-            <Checkout
-              name={'10+ Teachers'}
-              description={'Subscribe to get more teachers'}
-              amount={29.99}
-           />
+      <div className="page-container">
+        <Sidebar />
+        <div className="checkout right-side">
+          <h1>Upgrade Account</h1>
+          <Checkout
+            name={'10+ Teachers'}
+            description={'Subscribe to get more teachers'}
+            amount={this.fromDollarToCent(29.99)}
+            sendToken={subscribe}
+          />
           <h1>Pay a contractor</h1>
-          <p>Step 1: Send this link to your contractor via email to get them connected to our platform (needs to happen only once):</p>
-          <a href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_EotNW2K6Nn9nRDf9grdRR6gBaySaVZ3d&scope=read_write">https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_EotNW2K6Nn9nRDf9grdRR6gBaySaVZ3d&scope=read_write</a>
-           <p>Step 2: Fill out this form with the amount you're sending and the contractor's Stripe account number (starts with 'acct_').</p>
-           <form>
-            <input name="amount" placeholder="Amount to send" value={this.state.amount} onChange={this.handleChange}/>
-            <input name="accountId" placeholder="Contractor's Stripe Account ID" value={this.state.accountId} onChange={this.handleChange}/>
-           </form>
-            <p>Step 3: Pay</p>
-           <Connect
+          <p>
+            Step 1: Send this link to your contractor via email to get them
+            connected to our platform (needs to happen only once):
+          </p>
+          <a href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_EotNW2K6Nn9nRDf9grdRR6gBaySaVZ3d&scope=read_write">
+            https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_EotNW2K6Nn9nRDf9grdRR6gBaySaVZ3d&scope=read_write
+          </a>
+          <p>
+            Step 2: Fill out this form with the amount you're sending and the
+            contractor's Stripe account number (starts with 'acct_').
+          </p>
+          <form>
+            <input
+              name="amount"
+              placeholder="Amount to send"
+              value={this.state.amount}
+              onChange={this.handleChange}
+            />
+            <input
+              name="accountId"
+              placeholder="Contractor's Stripe Account ID"
+              value={this.state.accountId}
+              onChange={this.handleChange}
+            />
+          </form>
+          <p>Step 3: Pay</p>
+          <Checkout
             name="Pay a contractor"
             description=""
-            amount={Number(this.state.amount)}
+            amount={this.fromDollarToCent(Number(this.state.amount))}
             stripeAccount={this.state.accountId}
-           />
+            sendToken={connect}
+          />
         </div>
-     </div>
+      </div>
     );
-    
-}
+  }
 }
 
 export default Payments;
