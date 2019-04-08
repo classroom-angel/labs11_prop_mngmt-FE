5ba3dddce8efeb2aefbd349c7e680e4b819d3047
johnoro <officialjohnorourke@gmail.com>
2019-04-08 11:33:20 -0400

    Updates all formatting throughout with prettier

--- client/src/components/Testimonials/Testimonials.js
+++ client/src/components/Testimonials/Testimonials.js
@@ -1,69 +1,54 @@
 import React, { Component } from 'react';
 import axios from '../../axiosInstance';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import { Button, CardPanel, Row, Col } from 'react-materialize';
-import './testimonial.css'
+import './testimonial.css';
 
 class Testimonials extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        testimonials: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      testimonials: []
     };
-    
-    componentDidMount() {
-    console.log("hi hi!")
-    axios
-    .get("testimonials")
-    .then(response => {
-    console.log(response.data)
-    this.setState(function(){return{testimonials: [...response.data.testimonials]}}
-    );
-  })
-  .catch(error =>{
-    console.error("Cannot get testimonials",error);
-  })
-    }
+  }
 
- render(){
-     return(
-        <div className="testimonials">
-         <NavLink to="/AddTestimonial">Tell us what you think!</NavLink>
+  componentDidMount() {
+    console.log('hi hi!');
+    axios
+      .get('testimonials')
+      .then(response => {
+        console.log(response.data);
+        this.setState(function() {
+          return { testimonials: [...response.data.testimonials] };
+        });
+      })
+      .catch(error => {
+        console.error('Cannot get testimonials', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="testimonials">
+        <NavLink to="/AddTestimonial">Tell us what you think!</NavLink>
         {this.state.testimonials.map(testimonial => {
-            return (
-                
-             <Row>
-           <Col style={{margin: '0 auto'}} m={6} s={12}>
-           <CardPanel className="teal">
-           <span className="white-text">
-           <h5>{testimonial.name}</h5>
-           <h6>{testimonial.role}</h6> 
-           <p>{testimonial.text}</p>
-           </span>
-        </CardPanel>
-        </Col>
-        </Row>
-        )
+          return (
+            <Row>
+              <Col style={{ margin: '0 auto' }} m={6} s={12}>
+                <CardPanel className="teal">
+                  <span className="white-text">
+                    <h5>{testimonial.name}</h5>
+                    <h6>{testimonial.role}</h6>
+                    <p>{testimonial.text}</p>
+                  </span>
+                </CardPanel>
+              </Col>
+            </Row>
+          );
         })}
-       
-        </div>
-            
-       
-            
-       
-     )
- }
- 
+      </div>
+    );
+  }
 }
 
-
-
-
 export default Testimonials;
-
-
-
-
-
-
--- client/src/components/constants/stripe.js
+++ client/src/components/constants/stripe.js
@@ -1,5 +1,6 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_test_ZCDK4B5lWRJnQM5uJEqQgpf000rlo2uZtf'
-  : 'pk_test_ZCDK4B5lWRJnQM5uJEqQgpf000rlo2uZtf';
+const STRIPE_PUBLISHABLE =
+  process.env.NODE_ENV === 'production'
+    ? 'pk_test_ZCDK4B5lWRJnQM5uJEqQgpf000rlo2uZtf'
+    : 'pk_test_ZCDK4B5lWRJnQM5uJEqQgpf000rlo2uZtf';
 
 export default STRIPE_PUBLISHABLE;
