ae4051d2f76a71141c5996d0ae1c3248ea161866
Rachel DiCesare <racheldicesare93@gmail.com>
2019-04-09 10:31:06 -0400

    styled scheduled page

--- client/src/components/Scheduled/EditEventForm.js
+++ client/src/components/Scheduled/EditEventForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from '../axiosInstance';
+import axios from '../../axiosInstance';
 
 class EditEventForm extends Component {
   constructor(props) {
